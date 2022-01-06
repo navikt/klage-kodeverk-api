adb30210e78f5b8ceae8b712063b548b5e44e4f5
Øyvind Norsted Wedøe <oyvind.norsted.wedoe@nav.no>
2022-01-06 10:18:11 +0100

    Added actuator and management settings.

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,7 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.cloud:spring-cloud-starter-sleuth:$springSleuthVersion")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
