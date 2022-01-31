ac1b2b16d75eceee4907ee19bdee3836ff37f2ef
Andreas Jonsson <andreas.jonsson@nav.no>
2022-01-31 11:49:50 +0100

    Newest version of kodeverk

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("no.nav.security:token-validation-spring:$tokenValidationVersion")
 	implementation("no.nav.security:token-client-spring:$tokenValidationVersion")
-	implementation("com.github.navikt:kabal-kodeverk:2022.01.27-20.24.9b6588985739")
+	implementation("com.github.navikt:kabal-kodeverk:2022.01.28-13.10.43a8f316d7c7")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
