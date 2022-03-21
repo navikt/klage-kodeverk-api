8f500ebcf01a01f922d31332d66c352c7edd4c70
Øyvind Norsted Wedøe <oyvind.norsted.wedoe@nav.no>
2022-03-21 12:56:44 +0100

    Updated kodeverk version.

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("no.nav.security:token-validation-spring:$tokenValidationVersion")
 	implementation("no.nav.security:token-client-spring:$tokenValidationVersion")
-	implementation("com.github.navikt:kabal-kodeverk:2022.03.18-08.16.4c400b9ef7a8")
+	implementation("com.github.navikt:kabal-kodeverk:2022.03.21-12.30.b8382ea8d437")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
