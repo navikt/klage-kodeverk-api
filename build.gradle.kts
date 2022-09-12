bbe2306157ca64701a1dccc2227dbf2b56bae46b
Andreas Jonsson <andreas.jonsson@nav.no>
2022-09-12 14:59:12 +0200

    Newest kodeverk
    Co-authored-by: Øyvind Norsted Wedøe <oyvind.norsted.wedoe@nav.no>

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.springdoc:springdoc-openapi-ui:$springDocVersion")
-    implementation("com.github.navikt:kabal-kodeverk:bd75e2846e")
+    implementation("com.github.navikt:kabal-kodeverk:abf05e772a")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
