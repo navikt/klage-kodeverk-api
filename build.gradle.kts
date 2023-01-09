6be092f43ce8a035a8da196bf257be680ba06d4a
oyvindwedoe <oyvind.norsted.wedoe@nav.no>
2023-01-09 15:48:05 +0100

    Updated kodeverk.

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.springdoc:springdoc-openapi-ui:$springDocVersion")
-    implementation("com.github.navikt:klage-kodeverk:v1.2.0")
+    implementation("com.github.navikt:klage-kodeverk:v1.2.1")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
