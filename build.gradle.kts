d3f8610233426f975c850ee5a4ddb79dd3e811de
Andreas Jonsson <andreas.jonsson@nav.no>
2024-03-31 20:35:18 +0200

    Java 21 nonroot. Gradle 8.5.

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 
 apply(plugin = "io.spring.dependency-management")
 
-java.sourceCompatibility = JavaVersion.VERSION_17
+java.sourceCompatibility = JavaVersion.VERSION_21
 
 repositories {
     mavenCentral()
@@ -36,7 +36,7 @@ dependencies {
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "17"
+        jvmTarget = "21"
     }
 }
 
