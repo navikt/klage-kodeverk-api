105a9fde3145df5e095d860da3b29c7603b7cd9f
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2024-11-04 14:03:05 +0100

    Use kodeverk rc with new type.
    Co-authored-by: Andreas Jonsson <andreas.jonsson@nav.no>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val springDocVersion = "2.6.0"
-val kodeverkVersion = "1.8.56"
+val kodeverkVersion = "1.8.58-rc.0"
 
 plugins {
     val kotlinVersion = "2.0.21"
