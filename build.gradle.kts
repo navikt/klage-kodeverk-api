9757a0b8be4e339fe032b3cd38474fcaac987cc3
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2024-12-10 11:20:32 +0100

    Updated kodeverk.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val springDocVersion = "2.6.0"
-val kodeverkVersion = "1.9.1"
+val kodeverkVersion = "1.9.2"
 
 plugins {
     val kotlinVersion = "2.0.21"
