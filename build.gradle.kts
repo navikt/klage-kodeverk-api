8a52e976f496b1a72b32e521b6207001a55e8f30
Andreas Jonsson <andreas.jonsson@nav.no>
2025-08-05 22:40:23 +0200

    New kodeverk.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val springDocVersion = "2.8.9"
-val kodeverkVersion = "1.10.20"
+val kodeverkVersion = "1.10.21"
 
 plugins {
     val kotlinVersion = "2.2.0"
