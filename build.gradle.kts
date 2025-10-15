517796f0ded16c134c78dfaaddaab6696982ecb7
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2025-10-15 13:50:39 +0200

    RC of ungdomsprogrammet kodeverk.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val springDocVersion = "2.8.11"
-val kodeverkVersion = "1.11.13"
+val kodeverkVersion = "1.11.14-rc.0"
 
 plugins {
     val kotlinVersion = "2.2.10"
