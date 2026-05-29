03d507e37de1d1a8523e7b05d61636d3e2e3b322
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2026-05-29 09:56:19 +0200

    Use new temaToYtelserCurrentlyInUse function.

--- src/main/kotlin/no/nav/klage/kodeverkapi/util/KodeverkResponseGenerator.kt
+++ src/main/kotlin/no/nav/klage/kodeverkapi/util/KodeverkResponseGenerator.kt
@@ -70,11 +70,7 @@ fun getTypeList() = Type.entries.toKodeverkSimpleDto()
 fun getSimpleYtelseList() = Ytelse.entries.toKodeverkSimpleDto()
 
 fun getSimpleYtelseListForTema(temaId: String): List<KodeverkSimpleDto> {
-    return if (temaId == Tema.FEI.id) {
-        Ytelse.entries.toKodeverkSimpleDto()
-    } else {
-        Ytelse.entries.filter { it.toTema().id == temaId }.toKodeverkSimpleDto()
-    }
+    return Tema.of(temaId).toYtelserCurrentlyInUse().toKodeverkSimpleDto()
 }
 
 fun getSourceList() = Source.entries.toKodeverkSimpleDto()
