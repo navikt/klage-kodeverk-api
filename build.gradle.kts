773424448a722113e54e8c0b3bf2d9d18d7f591c
Andreas Jonsson <andreas.jonsson@nav.no>
2026-09-16 17:13:04 +0200

    Update kodeverkVersion to 3.3.33

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
 
 val ktlintVersion = "1.8.0"
 val springDocVersion = "3.1.0"
-val kodeverkVersion = "3.3.31"
+val kodeverkVersion = "3.3.33"
 val logstashVersion = "9.0"
 
 plugins {
