229fe040264ef50fe4ccf7044cf8f9a1b2075bfe
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2026-09-24 09:43:49 +0200

    Kodeverk 3.3.36.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
 
 val ktlintVersion = "1.8.0"
 val springDocVersion = "3.1.0"
-val kodeverkVersion = "3.3.35"
+val kodeverkVersion = "3.3.36"
 val logstashVersion = "9.0"
 
 plugins {
